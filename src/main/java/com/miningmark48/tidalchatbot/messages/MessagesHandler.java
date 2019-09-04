fbbf27208a8891acead8a5541843e607957f034e
Mark <miningmark48@gmail.com>
2019-09-04 14:44:14 -0400

    Renamed InitializeMessages to MessagesHandler

--- src/main/java/com/miningmark48/tidalchatbot/messages/MessagesHandler.java
+++ src/main/java/com/miningmark48/tidalchatbot/messages/MessagesHandler.java
@@ -18,7 +18,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class InitializeMessages {
+public class MessagesHandler {
 
     private static String baseDirectory = "messages";
 
