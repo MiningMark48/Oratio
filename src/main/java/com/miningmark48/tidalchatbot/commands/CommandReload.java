2e59ce8c87ad837a4696480c965a7c5174e38a30
Mark <miningmark48@gmail.com>
2019-09-06 00:26:50 -0400

    Log message for reload command

--- src/main/java/com/miningmark48/tidalchatbot/commands/CommandReload.java
+++ src/main/java/com/miningmark48/tidalchatbot/commands/CommandReload.java
@@ -3,6 +3,7 @@ package com.miningmark48.tidalchatbot.commands;
 import com.miningmark48.tidalchatbot.commands.base.ICommand;
 import com.miningmark48.tidalchatbot.handler.HandlerMessages;
 import com.miningmark48.tidalchatbot.reference.Reference;
+import com.miningmark48.tidalchatbot.util.UtilLogger;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class CommandReload implements ICommand {
@@ -18,6 +19,7 @@ public class CommandReload implements ICommand {
             event.getTextChannel().sendMessage("Reloading...").queue(m -> {
                 HandlerMessages.init();
                 m.editMessage("Reloaded!").queue();
+                UtilLogger.INFO.log(String.format("Reload! (by %s)", event.getAuthor().getAsTag()));
             });
         } else {
             event.getTextChannel().sendMessage(String.format("Sorry %s, you do not have permission to use that command.", event.getAuthor().getAsMention())).queue();
