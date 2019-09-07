d6bb27661e364e57fbb1e7b3dfbd2725a6f65e19
Mark <miningmark48@gmail.com>
2019-09-06 20:10:35 -0400

    Added colors to logger

--- src/main/java/com/miningmark48/tidalchatbot/util/UtilLogger.java
+++ src/main/java/com/miningmark48/tidalchatbot/util/UtilLogger.java
@@ -4,19 +4,33 @@ import com.miningmark48.tidalchatbot.TidalChatbot;
 
 public enum UtilLogger {
 
-    INFO,
-    DEBUG,
-    WARN,
-    FATAL,
-    STATUS;
+    INFO("\u001B[34m"),
+    DEBUG("\u001B[36m"),
+    WARN("\u001B[33m"),
+    FATAL("\u001B[31m"),
+    STATUS("\u001B[32m");
+
+    private String color = getReset();
 
     UtilLogger() {
 
     }
 
+    UtilLogger(String color) {
+        this.color = color;
+    }
+
+    private String getColor() {
+        return color;
+    }
+
+    private String getReset() {
+        return "\u001B[0m";
+    }
+
     public void log(String message) {
         if (this == DEBUG && !TidalChatbot.isDebugMode()) return;
-        System.out.printf("[%s] [%s] %s\n", UtilTime.getTimeHMS(), this.name().toUpperCase(), message);
+        System.out.printf("%s[%s] [%s] %s%s\n", getColor(), UtilTime.getTimeHMS(), this.name().toUpperCase(), message, getReset());
     }
 
 }
